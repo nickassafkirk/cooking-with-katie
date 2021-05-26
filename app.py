c637cfd3d9447b404116af9b116b4b82ca4585f2
nickassafkirk <nickassafkirk@gmail.com>
2021-05-26 21:50:05 +0000

    Configure app

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
 import os
-import pymongo
-from flask import Flask
-
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
 
 
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 MONGO_URI = os.environ.get("MONGO_URI")
-DATABASE = "katieDB"
+DATABASE = os.environ.get("MONGO_DBNAME")
 RECIPES = "recipes"
 
 
-def mongo_connect(url):
-    try:
-        conn = pymongo.MongoClient(url)
-        print("MongoDB is connected")
-        return conn
-    except pymongo.errors.ConnectionFailure as e:
-        print("Could not connect to MongoDB: %s") % e
-
-
-conn = mongo_connect(MONGO_URI)
-
-coll = conn[DATABASE][RECIPES]
-
-all_recipes = coll.find()
-
 for recipe in all_recipes:
     print(recipe)
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=(os.environ.get("PORT")),
+            debug=True)  # change debug to false before deploying
