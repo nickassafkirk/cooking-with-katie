1c2e7448b19c4e8c397f0ee32a268ca548eaf31c
nickassafkirk <nickassafkirk@gmail.com>
2021-05-26 18:46:36 +0000

    Connect application to database

--- app.py
+++ app.py
@@ -1,2 +1,30 @@
 import os
+import pymongo
 from flask import Flask
+
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "katieDB"
+RECIPES = "recipes"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("MongoDB is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][RECIPES]
+
+all_recipes = coll.find()
+
+for recipe in all_recipes:
+    print(recipe)
