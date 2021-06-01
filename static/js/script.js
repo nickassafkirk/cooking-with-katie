48a7eba6e13a1cdfdd8c06eae401a0a09919bff0
nickassafkirk <nickassafkirk@gmail.com>
2021-06-01 19:03:17 +0000

    update selector id names

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,21 @@
 
-const addRowsButton = document.getElementById("addRows");
-addRowsButton.addEventListener("click", addMoreRows);
+const addIngredientsButton = document.getElementById("addIngredientsButton");
+addIngredientsButton.addEventListener("click", addMoreRows);
 
 function addMoreRows() {
     console.log("Button Pressed")
     let numberOfIngredients = document.querySelectorAll(".ingredient-n").length;
     console.log(numberOfIngredients);
     let newIngredientRow = document.createElement("div");
-    newIngredientRow.setAttribute("class", "row ingredient-n");
+    newIngredientRow.setAttribute("class", "ingredient-n row");
     newIngredientRow.innerHTML= `
                     <div class="col">
                         <label for="ingredient-${numberOfIngredients+1}">Ingredient</label>
                         <input type="text" name="ingredient-${numberOfIngredients+1}">
                     </div>
                     <div class="col">
-                        <label for="ingredient-quantity-${numberOfIngredients+1}">Weight (Optional)</label>
-                        <input type="text" name="ingredient-quantity-${numberOfIngredients+1}">
+                        <label for="ingredient-quantity-${numberOfIngredients+1}">Weight</label>
+                        <input type="text" name="ingredient-quantity-${numberOfIngredients+1}" placeholder="Optional">
                     </div>
                 </div>`;  
     console.log(newIngredientRow);
