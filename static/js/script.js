056308cca67cf1fe97e7ed006421752612eec6a7
nickassafkirk <nickassafkirk@gmail.com>
2021-06-01 22:35:33 +0000

    Insert new ingredients input row on click

--- static/js/script.js
+++ static/js/script.js
@@ -3,9 +3,7 @@ const addIngredientsButton = document.getElementById("addIngredientsButton");
 addIngredientsButton.addEventListener("click", addMoreRows);
 
 function addMoreRows() {
-    console.log("Button Pressed")
     let numberOfIngredients = document.querySelectorAll(".ingredient-n").length;
-    console.log(numberOfIngredients);
     let newIngredientRow = document.createElement("div");
     newIngredientRow.setAttribute("class", "ingredient-n row");
     newIngredientRow.innerHTML= `
@@ -17,16 +15,14 @@ function addMoreRows() {
                         <label for="ingredient-quantity-${numberOfIngredients + 1}">Weight</label>
                         <input type="text" name="ingredient-quantity-${numberOfIngredients + 1}" placeholder="Optional">
                     </div>`;  
-    console.log(newIngredientRow);
     const ingredientsContainer = document.getElementById("ingredientsContainer");
-    console.log(ingredientsContainer.lastChild);
+    ingredientsContainer.insertBefore(newIngredientRow, document.querySelector(".button-container"));
 };
 
 const addInstructionsButton = document.getElementById("addInstructionsButton");
 addInstructionsButton.addEventListener("click", addInstructionRows);
 
 function addInstructionRows(){
-    console.log("Test why is this happening?")
     let numberOfInstructions = document.querySelectorAll(".instruction-n").length;
     let newInstructionRow = document.createElement("div");
     newInstructionRow.setAttribute("class", "instruction-n")
@@ -34,5 +30,5 @@ function addInstructionRows(){
         <label for="step-${numberOfInstructions + 1}" class="instructions-n__label">Step ${numberOfInstructions + 1}:</label>
         <textarea name="step-${numberOfInstructions + 1}" id="step-${numberOfInstructions + 1}" cols="50" rows="1"></textarea>`
     const instructionsContainer = document.getElementById("instructionsContainer");
-    console.log(instructionsContainer.lastChild);
+    
 };
