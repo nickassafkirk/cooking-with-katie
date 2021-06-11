a63d9459814e604ce24dd7193675160a8c9fc8be
nickassafkirk <nickassafkirk@gmail.com>
2021-06-11 21:34:24 +0000

    Enable form on click to prevent accidental data changes

--- static/js/account.js
+++ static/js/account.js
@@ -2,7 +2,14 @@ const accountDetails = document.querySelector("#editDetails");
 accountDetails.addEventListener("click", enableForm);
 
 function enableForm(){
+    accountDetails.classList.add("d-none");
     const formInputs = document.querySelectorAll("#account-update-form input");
     console.log(formInputs);
     formInputs.forEach(input => input.removeAttribute("disabled"));
+    updateAccountButton = document.querySelector("#update_account_button");
+    updateAccountButton.classList.remove("d-none");
+    cancelUpdateButton = document.querySelector("#cancel_update_button");
+    cancelUpdateButton.classList.remove("d-none");
+
+    
 }
