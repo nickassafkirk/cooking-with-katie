a345f1e614ac970c163b0ddef6d3d7625f1add63
nickassafkirk <nickassafkirk@gmail.com>
2021-06-11 21:46:05 +0000

    Use consistent syntax for 'disabled' attribute

--- static/js/account.js
+++ static/js/account.js
@@ -5,7 +5,7 @@ function enableForm(){
     accountDetails.classList.add("d-none");
     const formInputs = document.querySelectorAll("#account-update-form input");
     console.log(formInputs);
-    formInputs.forEach(input => input.removeAttribute("disabled"));
+    formInputs.forEach(input => input.disabled = false);
     updateAccountButton = document.querySelector("#update_account_button");
     updateAccountButton.classList.remove("d-none");
     cancelUpdateButton = document.querySelector("#cancel_update_button");
